140319bd3b122214c056145fa854ca1ae28a06d8
PSousa <p4sousa@gmail.com>
2015-04-27 18:09:17 +0100

    createItem

--- Website/database/userID.php
+++ Website/database/userID.php
@@ -6,7 +6,7 @@
     $stmt = $conn->prepare("SELECT idUtilizador , utilizador, nomeProprio FROM Utilizador WHERE utilizador = ?");
     $stmt->execute(array($_SESSION['username']));
 
-    $row = $stmt->fetch()
+    $row = $stmt->fetch();
 
     $_SESSION['idUser'] = $row['idUtilizador'];
     $_SESSION['nomeProprio'] = $row['nomeProprio'];
