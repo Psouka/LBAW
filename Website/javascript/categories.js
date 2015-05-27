d4951d1b54c91c2a128b5df1914bc9a5ad2a7c98
Ivo Lima da Silva <ivo.lima.da.silva@gmail.com>
2015-05-27 13:03:57 +0100

    Bugfix - Dropdown das categorias
    Faltava meter fechado logo de início caso a página fosse acedida de um
    telemóvel.

--- Website/javascript/categories.js
+++ Website/javascript/categories.js
@@ -1,3 +1,16 @@
+$(window).ready(function() {
+	if ($(this).width() < 768)
+	{
+		$('#collapseFour').removeClass('in');
+		$('#collapseFour').addClass('out');
+	}
+	else
+	{
+		$('#collapseFour').removeClass('out');
+		$('#collapseFour').addClass('in');
+	}
+});
+
 $(window).resize(function() {
 	if ($(this).width() < 768)
 	{
