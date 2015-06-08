ecf2c9faa4dc5b99f3d02b8f545fbf0fef50d83b
Boomsin <ei12190@fe.up.pt>
2015-06-08 14:46:10 +0100

    message database, falta pagina

--- Website/database/message.php
+++ Website/database/message.php
@@ -11,25 +11,43 @@ function createMessage ($idReceptor, $assunto, $texto)
     $stmt->execute(array($_SESSION['userid'], $idReceptor, $assunto, $texto);
 }
 
-function getMessageSend ()
+function getMessageSend ($numMessages, $offset)
 {
     global $conn;
     $stmt = $conn->prepare
     ("
-        SELECT * FROM message WHERE idemissor = ?
+        SELECT * FROM mensagem 
+        WHERE idemissor = ?
+        ORDER BY data
+        LIMIT ? OFFSET ?
         ");
-    $stmt->execute(array($_SESSION['userid']);
+    $stmt->execute(array($_SESSION['userid'], $numMessages, $offset);
     return $stmt->fetchAll();
 }
 
-function getMessageReceived ()
+function getMessageReceive ($numMessages, $offset)
 {
     global $conn;
     $stmt = $conn->prepare
     ("
-        SELECT * FROM message WHERE idreceptor = ?
+        SELECT * FROM mensagem 
+        WHERE idreceptor = ?
+        ORDER BY data
+        LIMIT ? OFFSET ?
         ");
-    $stmt->execute(array($_SESSION['userid']);
+    $stmt->execute(array($_SESSION['userid'], $numMessages, $offset);
+    return $stmt->fetchAll();
+}
+
+function getMessage ($idMessage)
+{
+    global $conn;
+    $stmt = $conn->prepare
+    ("
+        SELECT * FROM mensagem 
+        WHERE idmensagem = ?
+        ");
+    $stmt->execute(array($idMessage);
     return $stmt->fetchAll();
 }
 
